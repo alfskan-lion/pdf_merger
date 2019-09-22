c076e3df278929a78633a8c573905b320b600f58
leehyojun <alfskan@gmail.com>
2019-09-22 13:20:21 +0000

    update prefix

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,6 +1,7 @@
 class PdfsController < ApplicationController
+  before_action :set_pdfs, only: [:selects, :merge, :download_pdf]
+  
   def index
-    @pdfs = Pdf.all
   end
   
   def new
@@ -8,26 +9,33 @@ class PdfsController < ApplicationController
   end
   
   def create
+    if params[:pdf] == nil 
+      return redirect_to new_pdf_path 
+    end
     @pdf = Pdf.new(pdf_params)
     
       if @pdf.save
-         redirect_to merge_path(@pdf.id), notice: "The pdf has been combined."
+         redirect_to selects_path(@pdf.id), notice: "The pdf has been combined."
       else
          render :new
       end
   end
-  
-  def merge
-    @pdf = Pdf.find(params[:id])
-    pdf = CombinePDF.new
-    pdf << CombinePDF.load("public/uploads/pdf/pdf/#{params[:id]}/#{@pdf.pdf[0].identifier}") 
-    pdf << CombinePDF.load("public/uploads/pdf/pdf/#{params[:id]}/#{@pdf.pdf[1].identifier}")
-    pdf.save "public/uploads/pdf/pdf/#{params[:id]}/combined.pdf"
-    redirect_to download_path(params[:id]), notice: "The pdf has been combined."
+
+  def selects
+    @id = params[:id]
   end
   
-  def selects
-    @pdf = Pdf.find(params[:id])
+  def merge
+    index = params[:index].to_i
+    pdf = CombinePDF.new
+    unless index == 0 
+      @pdfs.insert(0, @pdfs.delete_at(index))
+    end
+    for i in 0..@pdfs.length-1
+      pdf << CombinePDF.load("public/uploads/pdf/pdf/#{params[:id]}/#{@pdfs[i].identifier}") 
+    end
+    pdf.save "public/uploads/pdf/pdf/#{params[:id]}/#{@pdfs.first.file.basename}_combined.pdf"
+    redirect_to download_path(params[:id])
   end
   
   def download
@@ -36,8 +44,8 @@ class PdfsController < ApplicationController
   
   def download_pdf
     send_file(
-    "public/uploads/pdf/pdf/#{params[:id]}/combined.pdf",
-    filename: "combined.pdf",
+    "public/uploads/pdf/pdf/#{params[:id]}/#{@pdfs.first.file.basename}_combined.pdf",
+    filename: "#{@pdfs.first.file.basename}_combined.pdf",
     type: "application/pdf"
     )
   end
@@ -46,7 +54,11 @@ class PdfsController < ApplicationController
   end
   
   private
-      def pdf_params
-        params.require(:pdf).permit({pdf: []})
-      end
+    def set_pdfs
+        @pdfs = Pdf.find(params[:id]).pdf
+    end
+    
+    def pdf_params
+      params.require(:pdf).permit({pdf: []})
+    end
 end
